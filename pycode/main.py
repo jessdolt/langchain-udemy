5cb981ed6d0dcdb43dd1a978b0f60c6b5ec01080
Jess Roque <68854057+jessdolt@users.noreply.github.com>
2024-03-13 14:33:10 +0800

    first commit

---
+++ pycode/main.py
@@ -0,0 +1,56 @@
+from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain, SequentialChain
+import argparse
+
+parser= argparse.ArgumentParser()
+parser.add_argument("--task", default="return a list of numbers")
+parser.add_argument('--language', default="python")
+args = parser.parse_args()
+
+
+api_key = "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"
+
+
+llm = OpenAI(
+    openai_api_key=api_key
+)
+
+code_prompt = PromptTemplate(
+    input_variables=['language', 'task'],
+    template="""Write a very so short {language} function that will {task}"""
+) 
+
+test_prompt = PromptTemplate(
+    input_variables=['language', 'code'],
+    template="""Write a test for the following  {language}  code:\n {code}""",
+)
+
+
+code_chain = LLMChain(
+    llm=llm,
+    prompt=code_prompt,
+    output_key='code'
+)
+
+test_chain = LLMChain(
+    llm=llm,
+    prompt=test_prompt,
+    output_key='test'
+)
+
+chain= SequentialChain(
+    chains=[code_chain, test_chain],
+    input_variables=["task", "language"],
+    output_variables=['test', "code"] 
+)
+
+result = chain({
+    "language": args.language,
+    "task": args.task
+})
+
+print(">>>>> GENERATED CODE: ")
+print(result['code'])
+print(">>>>>GENERATED TEST")
+print(result['test'])
